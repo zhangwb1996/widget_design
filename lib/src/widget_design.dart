29ac1520bd5b0e0f78c02e503f4bc5bb046269e4
zhangwb <804158121@qq.com>
2023-02-12 15:26:37 +0800

    > init

---
+++ lib/src/widget_design.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class WidgetDesign extends StatefulWidget {
+  const WidgetDesign({super.key});
+
+  @override
+  State<WidgetDesign> createState() => _WidgetDesignState();
+}
+
+class _WidgetDesignState extends State<WidgetDesign> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
