92b4dd83496eea52bb0b5f3f0c736bc3fb2493ec
zhangwb <804158121@qq.com>
2023-02-15 11:43:02 +0800

    Top-level provider to remain the state of design

--- lib/src/views/animation_and_motion/animated_align_view.dart
+++ lib/src/views/animation_and_motion/animated_align_view.dart
@@ -1,16 +1,16 @@
 ///
-/// File: \lib\src\views\animation_and_motion\animated_align_view.dart
-/// Project: widget_design
+/// File: \lib\src\views\animation_and_motion\animated_align_view.dart          
+/// Project: widget_design                                                      
 /// -----
 /// Created Date: Sunday, 2023-02-12 11:01:08 pm
 /// Author: Wenbo Zhang (zhangwb1996@outlook.com)
 /// -----
-/// Last Modified: Tuesday, 2023-02-14 11:23:54 pm
-/// Modified By: Wenbo Zhang (zhangwb1996@outlook.com)
+/// Last Modified: Wednesday, 2023-02-15 11:27:43 am                            
+/// Modified By: Wenbo Zhang (zhangwb1996@outlook.com)                          
 /// -----
 /// Copyright (c) 2023
 /// -----
-/// HISTORY:
+/// HISTORY:                                                                    
 /// Date      	By	Comments
 /// ----------	---	---------------------------------------------------------
 ///
@@ -29,55 +29,52 @@ class AnimatedAlignView extends StatelessWidget {
   // bool selected = false;
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (BuildContext context) => AnimatedAlignModel(),
-      child: Row(
-        children: [
-          // main content
-          Expanded(
-            child: Column(
-              children: [
-                // widget view
-                const Expanded(child: AnimatedAlignWidget()),
-                const Divider(),
-                // syntax highlighting
-                // TODO: show dart code with syntax highlighting and copy code by one key
-                Selector<AnimatedAlignModel, String>(
-                  selector: (_, animProperties) => animProperties.code,
-                  builder: (context, code, child) => SyntaxHighlight(
-                    code: code,
-                  ),
+    return Row(
+      children: [
+        // main content
+        Expanded(
+          child: Column(
+            children: [
+              // widget view
+              const Expanded(child: AnimatedAlignWidget()),
+              const Divider(),
+              // syntax highlighting
+              // TODO: show dart code with syntax highlighting and copy code by one key
+              Selector<AnimatedAlignModel, String>(
+                selector: (_, animProperties) => animProperties.code,
+                builder: (context, code, child) => SyntaxHighlight(
+                  code: code,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          // divider
-          const VerticalDivider(),
-          // designer
-          SizedBox(
-            width: 300,
-            child: Column(
-              children: [
-                // const CheckboxDesigner(),
-                // const RadioDesigner(),
-                Selector<AnimatedAlignModel, List<AlignmentGeometry>>(
-                  selector: (_, animProperties) => AlignmentList.listAlignment,
-                  builder: (context, list, child) => DropdownDesignerAlignment(
-                    items: list,
-                  ),
+        ),
+        // divider
+        const VerticalDivider(),
+        // designer
+        SizedBox(
+          width: 300,
+          child: Column(
+            children: [
+              // const CheckboxDesigner(),
+              // const RadioDesigner(),
+              Selector<AnimatedAlignModel, List<AlignmentGeometry>>(
+                selector: (_, animProperties) => AlignmentList.listAlignment,
+                builder: (context, list, child) => DropdownDesignerAlignment(
+                  items: list,
                 ),
-                Selector<AnimatedAlignModel, Map<String, Curve>>(
-                  selector: (_, animProperties) => CurveMap.mapCurve,
-                  builder: (context, list, child) => DropdownDesignerCurve(
-                    items: list,
-                  ),
+              ),
+              Selector<AnimatedAlignModel, Map<String, Curve>>(
+                selector: (_, animProperties) => CurveMap.mapCurve,
+                builder: (context, list, child) => DropdownDesignerCurve(
+                  items: list,
                 ),
-                const NumberDesigner(),
-              ],
-            ),
+              ),
+              const NumberDesigner(),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
