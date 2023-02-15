fa24b85cc08561aba12f295d9a7755b533b217d0
zhangwb <804158121@qq.com>
2023-02-15 23:31:12 +0800

    Added animated_list folder

---
+++ lib/src/preview/README.md
@@ -0,0 +1,3 @@
+# widget preview
+
+most code in current directory used from the [flutter doc](https://docs.flutter.dev/development/ui/widgets)
--- lib/src/preview/animation_and_motion/widget.dart
+++ lib/src/preview/animation_and_motion/widget.dart
@@ -5,7 +5,7 @@
 /// Created Date: Wednesday, 2023-02-15 10:19:54 pm
 /// Author: Wenbo Zhang (zhangwb1996@outlook.com)
 /// -----
-/// Last Modified: Wednesday, 2023-02-15 10:22:47 pm
+/// Last Modified: Wednesday, 2023-02-15 11:17:10 pm
 /// Modified By: Wenbo Zhang (zhangwb1996@outlook.com)
 /// -----
 /// Copyright (c) 2023
@@ -15,4 +15,7 @@
 /// ----------	---	---------------------------------------------------------
 ///
 
+export 'animated_list/animated_grid_preview.dart';
 export 'animated_list/animated_list_preview.dart';
+export 'animated_list/sliver_animated_grid_preview.dart';
+export 'animated_list/sliver_animated_list_preview.dart';
