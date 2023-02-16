00d91fa427545c1d79348f40bcc7080388464e23
zhangwb <804158121@qq.com>
2023-02-16 09:36:04 +0800

    Fixed warning: export the duplicated class

--- lib/src/preview/animation_and_motion/widget.dart
+++ lib/src/preview/animation_and_motion/widget.dart
@@ -5,7 +5,7 @@
 /// Created Date: Wednesday, 2023-02-15 10:19:54 pm
 /// Author: Wenbo Zhang (zhangwb1996@outlook.com)
 /// -----
-/// Last Modified: Wednesday, 2023-02-15 11:17:10 pm
+/// Last Modified: Thursday, 2023-02-16 9:24:21 am
 /// Modified By: Wenbo Zhang (zhangwb1996@outlook.com)
 /// -----
 /// Copyright (c) 2023
@@ -16,6 +16,8 @@
 ///
 
 export 'animated_list/animated_grid_preview.dart';
-export 'animated_list/animated_list_preview.dart';
-export 'animated_list/sliver_animated_grid_preview.dart';
-export 'animated_list/sliver_animated_list_preview.dart';
+export 'animated_list/animated_list_preview.dart' show AnimatedListPreview;
+export 'animated_list/sliver_animated_grid_preview.dart'
+    show SliverAnimatedGridPreview;
+export 'animated_list/sliver_animated_list_preview.dart'
+    show SliverAnimatedListPreview;
